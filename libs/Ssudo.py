3cb5e794ed2e0d4effa92afe1758c1235e7ec366
Cleiton Lima <cleitonlima@gawab.com>
2011-01-16 21:39:52 -0200

    First Release

---
+++ libs/Ssudo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/ env python
+
+#Setup sudo in Fedora
+#Cleiton Lima
+#License: GPLv3
+
+from os import getlogin, getenv, chdir
+
+line = str("\n")
+user = str(getlogin())
+print user
+cmd = str(" ALL=(ALL) NOPASSWD:ALL")
+chdir(str("/home/cleiton/"))
+sudoers = open ('sudoers', 'r').readlines()
+sudoers.append(line+user+cmd)
+sudo = open('sudoers', 'w')
+sudo.writelines(sudoers)
+sudo.close
