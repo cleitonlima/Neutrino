b040221c82f895dc566bbd1675f610a9717ac060
cleitonlima <cleitonlima@gawab.com>
2011-05-29 18:52:34 -0300

    Ssudo.py bugs solved

--- stable/Neutrino/libs/Ssudo.py
+++ stable/Neutrino/libs/Ssudo.py
@@ -19,18 +19,19 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Neutrino.  If not, see <http://www.gnu.org/licenses/>.
 
-#Add Nouveau to BlackList
-#Used by nvidia.py module
-#Must be executed as root
+#Setup sudo in Fedora
+from os import getlogin, getenv, chdir
 
-file = open("/boot/grub/grub.conf", "r").readlines()
-for position, line in enumerate(file):
-	if "kernel /vmlinuz-2" in line:
-		text = str(' rdblacklist=nouveau')
-...             br = str('\n')
-...             sline = line.split("\n")
-...             nline= str(str(sline[0])+text+br)
-...             file[position] = nline
-open("/boot/grub/grub.conf", "w").writelines(file)
+line = str("\n")
+user = str(getlogin())
+print user
+cmd = str(" ALL=(ALL) NOPASSWD:ALL")
+chdir(str("/etc/"))
+sudoers = open ('sudoers', 'r').readlines()
+sudoers.append(line+user+cmd)
+sudo = open('sudoers', 'w')
+sudo.writelines(sudoers)
+sudo.close
+		
 
-
+SUDO_DESCRIPTION = str("Configurar o uso do Sudo no Fedora.")
